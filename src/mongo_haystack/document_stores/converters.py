686c0ea6a9ee0eb52f454fdc01c1ae7600ef728c
mtwomey <mtwomey@beakstar.com>
2023-11-06 15:42:18 -0600

    Ensure embedding is present in Document regardless

--- src/mongo_haystack/document_stores/converters.py
+++ src/mongo_haystack/document_stores/converters.py
@@ -1,13 +1,23 @@
 from haystack.schema import Document
+from typing import Dict
 
 
-def mongo_doc_to_hystack_doc(mongo_doc):
+def mongo_doc_to_hystack_doc(mongo_doc) -> Document:
+    if "embedding" in mongo_doc:
+        embedding = mongo_doc["embedding"]
+    else:
+        embedding = None
+
     return Document(
-        id=mongo_doc["id"], content=mongo_doc["content"], content_type=mongo_doc["content_type"], meta=mongo_doc["meta"]
+        id=mongo_doc["id"],
+        content=mongo_doc["content"],
+        content_type=mongo_doc["content_type"],
+        meta=mongo_doc["meta"],
+        embedding=embedding,
     )
 
 
-def haystack_doc_to_mongo_doc(haystack_doc):
+def haystack_doc_to_mongo_doc(haystack_doc) -> Dict:
     return {
         "id": haystack_doc.id,
         "content": haystack_doc.content,
