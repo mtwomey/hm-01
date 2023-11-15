5a2592ff90f80b97bc8efa6917c1ef076874d057
mtwomey <mtwomey@beakstar.com>
2023-11-15 00:19:03 -0600

    Demo app

---
+++ demo/80_days/demo.py
@@ -0,0 +1,73 @@
+import os
+from haystack.nodes import AnswerParser, EmbeddingRetriever, PromptNode, PromptTemplate
+from haystack.pipelines import Pipeline
+from mongo_haystack import MongoAtlasDocumentStore
+import warnings
+
+# Ignore all warnings
+warnings.filterwarnings("ignore")  # Supress the pytorch warning
+
+openapi_key = os.getenv("OPENAI_KEY")
+
+mongo_atlas_username = os.getenv("MONGO_ATLAS_USERNAME")
+mongo_atlas_password = os.getenv("MONGO_ATLAS_PASSWORD")
+mongo_atlas_host = os.getenv("MONGO_ATLAS_HOST")
+mongo_atlas_database = os.getenv("MONGO_ATLAS_DATABASE")
+mongo_atlas_collection = "80_days"
+mongo_atlas_connection_params = {"retryWrites": "true", "w": "majority"}
+mongo_atlas_params_string = "&".join([f"{key}={value}" for key, value in mongo_atlas_connection_params.items()])
+mongo_atlas_connection_string = (
+    f"mongodb+srv://{mongo_atlas_username}:{mongo_atlas_password}@{mongo_atlas_host}/?{mongo_atlas_params_string}"
+)
+
+document_store = MongoAtlasDocumentStore(
+    mongo_connection_string=mongo_atlas_connection_string,
+    database_name=mongo_atlas_database,
+    collection_name=mongo_atlas_collection,
+    embedding_dim=768,
+    progress_bar=False,
+)
+
+retriever = EmbeddingRetriever(
+    document_store=document_store,
+    embedding_model="sentence-transformers/all-mpnet-base-v2",  # Recommended here: https://www.sbert.net/docs/pretrained_models.html
+    model_format="sentence_transformers",
+    top_k=10,
+    progress_bar=False,
+)
+
+
+def main():
+    print("\nWhat would you like to know about the book 'Around the World in Eighty Days' by Jules Verne?")
+    while True:
+        user_question = input("\nQuestion: ")
+        if user_question.lower() in ["exit", "quit"]:
+            break
+        response = get_response(user_question)
+        print(f"\n{response}\n")
+
+
+def get_response(user_question):
+    rag_prompt = PromptTemplate(
+        prompt="""Synthesize a comprehensive answer from the following text for the given question.
+                  Provide a clear and concise response that summarizes the key points and information presented in the text.
+                  Your answer should be in your own words and be no longer than 50 words.
+                  \n\n Related text: {join(documents)} \n\n Question: {query} \n\n Answer:""",
+        output_parser=AnswerParser(),
+    )
+    prompt_node = PromptNode(
+        model_name_or_path="gpt-3.5-turbo-instruct",
+        api_key=openapi_key,
+        default_prompt_template=rag_prompt,
+        max_length=150,
+    )
+    pipe = Pipeline()
+    pipe.add_node(component=retriever, name="retriever", inputs=["Query"])
+    pipe.add_node(component=prompt_node, name="prompt_node", inputs=["retriever"])
+
+    output = pipe.run(query=user_question)
+    return output["answers"][0].answer
+
+
+if __name__ == "__main__":
+    main()
---
+++ demo/80_days/setup.py
@@ -0,0 +1,86 @@
+import os
+import re
+import roman
+from haystack.nodes import EmbeddingRetriever, PreProcessor
+from haystack.schema import Document
+from mongo_haystack import MongoAtlasDocumentStore
+
+# Process the book text into Haystack Documents
+
+
+def get_book():
+    with open("80_days.txt", "r", encoding="utf-8") as file:
+        text = file.read()
+    return text
+
+
+def divide_book_into_chapters(book) -> dict:
+    lines = book.split("\n")
+    current_chapter = None
+    chapters = {}
+    for line in lines:
+        chapter_match = re.match(r"CHAPTER\s+([IVXLCDM]+)\.*", line)
+        if chapter_match:
+            chapter_roman = chapter_match.group(1)
+            chapter_decimal = roman.fromRoman(chapter_roman)
+            current_chapter = f"CHAPTER {chapter_decimal}".title()
+            chapters[current_chapter] = ""
+        if current_chapter:
+            chapters[current_chapter] += line + "\n"
+    return chapters
+
+
+chapters = divide_book_into_chapters(get_book())
+
+documents = [
+    Document(
+        content=chapters[f"Chapter {n}"],
+        meta={"book": "Around the World in 80 Days", "Chapter": n},
+    )
+    for n in range(1, len(chapters) + 1)
+]
+
+processor = PreProcessor(
+    clean_empty_lines=True,
+    clean_whitespace=True,
+    clean_header_footer=True,
+    remove_substrings=None,
+    split_by="word",
+    split_length=200,
+    split_respect_sentence_boundary=True,
+    split_overlap=0,
+    max_chars_check=10_000,
+)
+
+processed_documents = processor.process(documents)
+
+# Load the Documents into a Mongo Atlas Collection
+
+mongo_atlas_username = os.getenv("MONGO_ATLAS_USERNAME")
+mongo_atlas_password = os.getenv("MONGO_ATLAS_PASSWORD")
+mongo_atlas_host = os.getenv("MONGO_ATLAS_HOST")
+mongo_atlas_database = os.getenv("MONGO_ATLAS_DATABASE")
+mongo_atlas_collection = "80_days"
+mongo_atlas_connection_params = {"retryWrites": "true", "w": "majority"}
+mongo_atlas_params_string = "&".join([f"{key}={value}" for key, value in mongo_atlas_connection_params.items()])
+mongo_atlas_connection_string = (
+    f"mongodb+srv://{mongo_atlas_username}:{mongo_atlas_password}@{mongo_atlas_host}/?{mongo_atlas_params_string}"
+)
+
+document_store = MongoAtlasDocumentStore(
+    mongo_connection_string=mongo_atlas_connection_string,
+    database_name=mongo_atlas_database,
+    collection_name=mongo_atlas_collection,
+    embedding_dim=768,
+)
+
+document_store.write_documents(processed_documents)
+
+retriever = EmbeddingRetriever(
+    document_store=document_store,
+    embedding_model="sentence-transformers/all-mpnet-base-v2",  # Recommended here: https://www.sbert.net/docs/pretrained_models.html
+    model_format="sentence_transformers",
+    top_k=10,
+)
+
+document_store.update_embeddings(retriever, batch_size=60)
